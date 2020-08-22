f1f9e2ec0bcd8c7b29e0e1a849596a8acb62be57
HarrisonBoland <Harrisonheke@gmail.com>
2020-08-22 14:17:18 +1200

    Set up clickable User

--- react-frontend/src/user/pages/Users.js
+++ react-frontend/src/user/pages/Users.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-  return <h2>User's Works</h2>
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Harrison Boland',
+      image:
+        'https://www.shanethegamer.com/wp-content/uploads/2019/12/Halo-Reach.jpg',
+      places: 3
+    }
+  ];
+
+  return <UsersList items={USERS}/>;
 };
 
 export default Users;
